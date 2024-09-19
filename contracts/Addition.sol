a5dc1a5216ec15869b9bbbf7a2abdfb0a5cd5b89
Gravit09 <garvitchoudhary09@gmail.com>
2024-09-19 23:54:33 +0530

    contract deploy alternate script

--- contracts/Addition.sol
+++ contracts/Addition.sol
@@ -1,9 +1,8 @@
- // SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
 contract Addition {
-// Function to add two numbers and return the result
- function add(uint256 num1, uint256 num2) public pure returns (uint256) {
- return num1 + num2;
- }
- }
+    function add(uint256 a, uint256 b) public pure returns (uint256) {
+        return a + b;
+    }
+}
