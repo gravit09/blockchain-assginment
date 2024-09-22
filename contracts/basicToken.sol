9215a46e4a84b2921a3aac614e90a46854f81278
Gravit09 <garvitchoudhary09@gmail.com>
2024-09-22 23:34:52 +0530

    basic token contract

---
+++ contracts/basicToken.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+contract BasicTokenContract {
+    address private deployer;
+
+    mapping(address => uint) public balances;
+
+    event TokensMinted(address  owner, uint value);
+    event Transfer(address from, address to, uint value);
+
+    constructor() {
+        deployer = msg.sender;
+    }
+
+    modifier isOwner() {
+        require(msg.sender == deployer, "Not the contract owner");
+        _;
+    }
+
+    modifier isEnoughBalance(uint value) {
+        require(balances[msg.sender] >= value, "Insufficient balance");
+        _;
+    }
+
+    function mintToken(uint value) public {
+        balances[msg.sender] += value;
+        emit TokensMinted(msg.sender, value);
+    }
+
+    function transfer(uint value, address receiver) public isEnoughBalance(value) {
+        require(receiver != address(0), "Invalid receiver address");
+        balances[receiver] += value;
+        balances[msg.sender] -= value;
+        emit Transfer(msg.sender, receiver, value);
+    }
+}
